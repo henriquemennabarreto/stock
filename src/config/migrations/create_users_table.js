9ca01854994dc8dfa0952f6b9f6c183ad9561dc8
Henrique Menna Barreto <hmennabarretoaraujo@gmail.com>
2023-10-01 19:43:53 -0300

    update user migration

--- src/config/migrations/create_users_table.js
+++ src/config/migrations/create_users_table.js
@@ -1,6 +1,7 @@
 exports.up = function (knex) {
   return knex.schema.createTable('users', (table) => {
     table.increments('id').primary();
+    table.string('name').notNullable().unique();
     table.string('email').notNullable().unique();
     table.string('password').notNullable();
     table.timestamp('created_at').defaultTo(knex.fn.now());
