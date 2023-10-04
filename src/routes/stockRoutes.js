087492ff5119add1e63b44731c254604a464d13c
Henrique Menna Barreto <hmennabarretoaraujo@gmail.com>
2023-10-04 13:29:13 -0300

    stock and users routes

--- src/routes/stockRoutes.js
+++ src/routes/stockRoutes.js
@@ -4,18 +4,18 @@ const stockController = require('../controllers/stockController');
 const authController = require('../controllers/authController');
 
 // Rota para listar todos os produtos
-router.get('/stock',  authController.verifyToken, stockController.getAllItems);
+router.get('/stock', authController.hasActiveLogin, stockController.getAllItems);
 
 // Rota para obter um produto específico pelo ID
-router.get('/stock/:id', authController.verifyToken, stockController.getItemById);
+router.get('/stock/:id', authController.hasActiveLogin, stockController.getItemById);
 
 // Rota para adicionar um novo produto
-router.post('/stock', authController.verifyToken, stockController.addItem);
+router.post('/stock', authController.hasActiveLogin, stockController.addItem);
 
 // Rota para atualizar um produto específico pelo ID
-router.put('/stock/:id', authController.verifyToken, stockController.updateItem);
+router.put('/stock/:id', authController.hasActiveLogin, stockController.updateItem);
 
 // Rota para deletar um produto específico pelo ID
-router.delete('/stock/:id', authController.verifyToken, stockController.deleteItem);
+router.delete('/stock/:id', authController.hasActiveLogin, stockController.deleteItem);
 
 module.exports = router;
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,22 +4,30 @@ const userController = require('../controllers/userController');
 const authController = require('../controllers/authController');
 
 // Rota para logar
-router.post('/users/login', authController.generateToken);
+router.get('/google', authController.login);
+
+router.get('/google/callback', authController.callback);
+
+router.get('/logout', authController.logout);
+
+router.get('/success', authController.success);
+
+router.get('/failed', authController.failed);
 
 // Rota para listar todos os usuários
-router.get('/users', userController.getAllUsers);
+router.get('/users', hasActiveLogin, userController.getAllUsers);
 
 // Rota para obter um usuário específico pelo ID
-router.get('/users/:id', userController.getUserById);
+router.get('/users/:id', hasActiveLogin, userController.getUserById);
 
 // Rota para adicionar um novo usuário
-router.post('/users', userController.addUser);
+router.post('/users', hasActiveLogin, userController.addUser);
 
 // Rota para atualizar um usuário específico pelo ID
-router.put('/users/:id', userController.updateUser);
+router.put('/users/:id', hasActiveLogin, userController.updateUser);
 
 // Rota para deletar um usuário específico pelo ID
-router.delete('/users/:id', userController.deleteUser);
+router.delete('/users/:id', hasActiveLogin, userController.deleteUser);
 
 
 module.exports = router;
