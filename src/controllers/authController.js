71d10383d44aa598dbc45aef4f56c2e474f48b46
Henrique Menna Barreto <hmennabarretoaraujo@gmail.com>
2023-10-04 14:17:06 -0300

    jwt support

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,13 +1,51 @@
 const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const UserModel = require('../models/userModel');
 
-passport.initialize()
-passport.session()
+const SECRET = '448a852ec16449869e0101fa7ea9e0ad';
 
 const AuthController = {
 
+  generateToken: async (req, res) => {
+    try {
+      const { email, password } = req.body;
+
+      const user = await UserModel.getUserByEmail(email);
+      if (!user) {
+        return res.status(404).json({ message: "Usuário não encontrado." });
+      }
+
+      const passwordIsValid = await bcrypt.compare(password, user.password);
+      if (!passwordIsValid) {
+        return res.status(401).json({ message: "Senha inválida." });
+      }
+
+      const token = jwt.sign({ id: user.id }, SECRET, {
+        expiresIn: 86400 // Token expira em 24 horas
+      });
+
+      res.status(200).json({ token });
+    } catch (error) {
+      res.status(500).json({ message: "Erro ao gerar o token.", error });
+    }
+  },
+
   hasActiveLogin: async (req, res, next) => {
     try {
+      let token = req.headers['x-access-token'] || req.headers['authorization'];
+
       if (req.user) {
+        next();
+      } else if (token) {
+        if (token.startsWith('Bearer ')) {
+          token = token.slice(7, token.length);
+        }
+
+        const decoded = jwt.verify(token, SECRET);
+
+        req.userId = decoded.id;
+
         next();
       } else {
         res.status(401).json({ message: "Usuário não encontrado." });
@@ -24,7 +62,10 @@ const AuthController = {
   },
 
   success: async (req, res) => {
-    res.send(`Usuário ${req.user.email} logado com sucesso.`)
+    const token = jwt.sign({ id: req.user.email }, SECRET, {
+      expiresIn: 86400 // Token expira em 24 horas
+    });
+    res.status(200).json({ user: req.user.email, token });
   },
 
   failed: async (req, res) => {
