442c1ea37c448b8c1caf9838237319be7e516f06
Henrique Menna Barreto <hmennabarretoaraujo@gmail.com>
2023-10-04 13:28:50 -0300

    google auth controller

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,55 +1,42 @@
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
-const UserModel = require('../models/userModel');
-
-const SECRET = '448a852ec16449869e0101fa7ea9e0ad';
+const passport = require('passport');
 
 const AuthController = {
 
-  generateToken: async (req, res) => {
+  login: async (req, res) => {
     try {
-      const { email, password } = req.body;
-
-      const user = await UserModel.getUserByEmail(email);
-      if (!user) {
-        return res.status(404).json({ message: "Usuário não encontrado." });
-      }
-
-      const passwordIsValid = await bcrypt.compare(password, user.password);
-      if (!passwordIsValid) {
-        return res.status(401).json({ message: "Senha inválida." });
-      }
-
-      const token = jwt.sign({ id: user.id }, SECRET, {
-        expiresIn: 86400 // Token expira em 24 horas
-      });
-
-      res.status(200).json({ token });
+      passport.authenticate('google', {
+        scope:
+          ['email', 'profile']
+      })
     } catch (error) {
       res.status(500).json({ message: "Erro ao gerar o token.", error });
     }
   },
 
-  verifyToken: async (req, res, next) => {
+  hasActiveLogin: async (req, res, next) => {
     try {
-      let token = req.headers['x-access-token'] || req.headers['authorization'];
-
-      if (!token) {
-        return res.status(403).json({ message: "Nenhum token fornecido." });
+      if (req.user) {
+        next();
+      } else {
+        res.status(401).json({ message: "Usuário não encontrado." });
       }
-
-      if (token.startsWith('Bearer ')) {
-        token = token.slice(7, token.length);
-      }
-
-      const decoded = jwt.verify(token, SECRET);
-
-      req.userId = decoded.id;
-
-      next();
     } catch (error) {
       res.status(401).json({ message: "Não autorizado.", error });
     }
+  },
+
+  logout: async (req, res) => {
+    req.session = null;
+    req.logout();
+    res.redirect('/');
+  },
+
+  success: async (req, res) => {
+    res.send(`Usuário ${req.user.email} logado com sucesso.`)
+  },
+
+  failed: async (req, res) => {
+    res.status(401).json({ message: "Falha na autenticação.", error });
   }
 };
 
